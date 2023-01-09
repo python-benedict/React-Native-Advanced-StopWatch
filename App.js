994c8115ca1bbc41e737cbf903c7a8637de25e93
Benedict <newlyf38@gmail.com>
2023-01-09 08:20:48 +0000

    created a DATA object to store static timer and laps values

--- App.js
+++ App.js
@@ -1,6 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+const DATA = {
+  timer: 1234567,
+  laps : [ 231, 556, 235, 977 ],
+}
+
 export default function App() {
   return (
     <View style={styles.container}>
